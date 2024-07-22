890b27c686c7b7334566a7520d435856dcc2a17f
jmtpf <142712513+jmtpf@users.noreply.github.com>
2024-07-22 11:42:01 +0200

    feat: semantic base elements

---
+++ app/_components/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-start p-10 flex-wrap border border-red-500 mt-8">
+      Footer
+    </footer>
+  );
+};
+export default Footer;
---
+++ app/_components/header.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+const Header = () => {
+  return (
+    <header className="flex flex-col px-6 py-7 border-red-500 border">
+      Header
+    </header>
+  );
+};
+export default Header;
